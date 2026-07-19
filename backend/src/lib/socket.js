238bb880fb4e64e3e70631ffe769f3148bb3224d
Sʜɪᴠᴀɴɢ Sɪɴɢʜ <shivangsingh961@gmail.com>
2026-07-19 15:52:44 +0530

    Enhance socket.js with CORS credentials and user tracking
    Added credentials support for CORS and improved userSocketMap management.

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,6 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL,
+    credentials: true,
   },
 });
 
@@ -15,21 +16,22 @@ export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
+const userSocketMap = {};
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
+
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
+
     delete userSocketMap[userId];
+
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
